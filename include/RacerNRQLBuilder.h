37f23830e08a6a22af7be515e0e2fba77793052f
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2006-07-31 17:47:44 +0000

    Add documentation.

--- include/RacerNRQLBuilder.h
+++ include/RacerNRQLBuilder.h
@@ -23,42 +23,101 @@
 namespace dlvhex {
 namespace racer {
 
+  /**
+   * @brief Base class for all the NRQL Builders.
+   */
   class NRQLBuilder
   {
   public:
     virtual
     ~NRQLBuilder();
 
+    /** 
+     * Base class method does nothing.
+     *
+     * @param stream 
+     * @param query 
+     * 
+     * @return false
+     */
     virtual bool
     createBody(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
 
+    /** 
+     * Uses query's patterntuple to create a nRQL head.
+     * 
+     * @param stream output the head to this stream
+     * @param query use this Query
+     * 
+     * @return true, if method created an output, false otherwise.
+     */
     virtual bool
     createHead(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
 
+    /** 
+     * Uses query's interpretation and create a list of ABox
+     * assertions.
+     * 
+     * @param stream output the ABox assertions to this stream
+     * @param query use this query
+     * 
+     * @return true if method created an output, false otherwise.
+     */
     virtual bool
     createPremise(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
   };
 
 
+  /**
+   * @brief Builds conjunctive nRQL queries.
+   */
   class NRQLConjunctionBuilder : public NRQLBuilder
   {
   public:
+    /** 
+     * Uses query to build a conjunctive query expression.
+     * 
+     * @param stream output the conjunctive query to this stream
+     * @param query use this query
+     * 
+     * @return true if method created an output, false otherwise.
+     */
     virtual bool
     createBody(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
   };
 
 
+  /**
+   * @brief Builds datatype role queries.
+   */
   class NRQLDatatypeBuilder : public NRQLBuilder
   {
   public:
+    /** 
+     * Uses query to build the head expresision for a datatype role
+     * query.
+     * 
+     * @param stream output the head expression to this stream
+     * @param query  use this query
+     * 
+     * @return true
+     */
     virtual bool
     createHead(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
 
+    /** 
+     * Uses query to build a datatype role query.
+     * 
+     * @param stream output the datatype role query to this stream
+     * @param query  use this query
+     * 
+     * @return true
+     */
     virtual bool
     createBody(std::ostream& stream, const Query& query) const
       throw(RacerBuildingError);
