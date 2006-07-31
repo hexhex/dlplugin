e8f0b8deefb5ff10c72c9a438b5ab2014e658bdf
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2006-07-31 17:49:20 +0000

    Use yyRacerFlexLexer instead of yyFlexLexer.

--- include/RacerFlexLexer.h
+++ include/RacerFlexLexer.h
@@ -13,38 +13,35 @@
 #ifndef _RACERFLEXLEXER_H
 #define _RACERFLEXLEXER_H
 
-#include "RacerAnswerParser.hpp"
-
 // put FlexLexer.h into its own include guards or yyFlexLexer gets
 // redefined
 #ifndef __FLEX_LEXER_H
+#undef yyFlexLexer
+#define yyFlexLexer yyRacerFlexLexer
 #include <FlexLexer.h>
 #endif
 
-//
-// some forward declarations
-//
-
+/// forward declaration
 namespace dlvhex {
-namespace racer {
-  class RacerBaseAnswerDriver;
-} // namespace racer
+  namespace racer {
+    class RacerBaseAnswerDriver;
+  } // namespace racer
 } // namespace dlvhex
 
-  /**
-   * @brief Use a refined yyFlexLexer.
-   *
-   * Sad but true, we can't put RacerFlexLexer into the dlvhex::racer
-   * namespace, flex is not aware of namespaces.
-   */
-  struct RacerFlexLexer : public yyFlexLexer
-  {
-    RacerFlexLexer(dlvhex::racer::RacerBaseAnswerDriver* d) : lexdrv(d) { }
-    virtual ~RacerFlexLexer() { }
-    dlvhex::racer::RacerBaseAnswerDriver* lexdrv;
-    yy::RacerAnswerParser::location_type* lexloc;
-    yy::RacerAnswerParser::semantic_type* lexval;
-    int yylex(); // implemented in RacerAnswerScanner.lpp
-  };
+/**
+ * @brief Use a refined yyFlexLexer.
+ *
+ * Sad but true, we can't put RacerFlexLexer into the dlvhex::racer
+ * namespace, flex is not aware of namespaces.
+ */
+struct RacerFlexLexer : public yyRacerFlexLexer
+{
+  RacerFlexLexer(dlvhex::racer::RacerBaseAnswerDriver* d) : lexdrv(d) { }
+  virtual ~RacerFlexLexer() { }
+  dlvhex::racer::RacerBaseAnswerDriver* lexdrv;
+  yy::RacerAnswerParser::location_type* lexloc;
+  yy::RacerAnswerParser::semantic_type* lexval;
+  int yylex(); // implemented in RacerAnswerScanner.lpp
+};
 
 #endif // _RACERFLEXLEXER_H
