9f30e8b1dc352e1cff5a91d6034700cfab09dc70
Minh Dao-Tran <dao@kr.tuwien.ac.at>
2008-01-28 16:09:49 +0000

    Header file for Defauls.h

---
+++ include/Defaults.h
@@ -0,0 +1,37 @@
+/**
+ * @file   Defaults.h
+ * @author DAO Tran Minh
+ * @date   Tue Dec 18 17:20:24 2007
+ * 
+ * @brief  A set of Defaults.
+ * 
+ * 
+ */
+
+#ifndef _DLVHEX_DF_DEFAULTS_H
+#define _DLVHEX_DF_DEFAULTS_H
+
+#include <vector>
+#include "Default.h"
+#include "Updates.h"
+
+namespace dlvhex {
+namespace df {
+
+class Default;
+
+class Defaults {
+private:
+	std::vector<Default> dfs;
+public:
+	Defaults();
+	void addDefault(Default&);
+	Updates getLambda();
+	Updates getLambdaPrime();
+	DLRules getDLRules();
+};
+
+}	// namespace df
+}	// namespace dlvhex
+
+#endif /* _DLVHEX_DF_DEFAULTS_H */
