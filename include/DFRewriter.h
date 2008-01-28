f335d97e587e07d35b1fac6011e99567b90abac8
Minh Dao-Tran <dao@kr.tuwien.ac.at>
2008-01-28 16:11:21 +0000

    Header file for DFRewriter.cpp

---
+++ include/DFRewriter.h
@@ -0,0 +1,38 @@
+/**
+ * @file   DFRewriter.h
+ * @author DAO Tran Minh
+ * @date   Tue Dec 18 22:10:21 2007
+ * 
+ * @brief  A Rewriter from Defaults to DLRules.
+ * 
+ * 
+ */
+
+#ifndef _DLVHEX_DF_REWRITER_H
+#define _DLVHEX_DF_REWRITER_H
+
+#include "Ontology.h"
+#include "DefaultParser.h"
+
+namespace dlvhex { namespace df {
+
+class DFRewriter {
+private:
+	std::string input_file;
+	std::string ontology_uri;
+	std::string program;
+	std::vector<std::string> individuals;
+	void readIndividuals();
+	void readIndividuals(dlvhex::dl::Ontology::shared_pointer o);
+	void convert();
+public:
+	DFRewriter(std::string&, std::string&);
+	DFRewriter();
+	void transform(std::istream&, std::ostream&);
+	void transform(std::istream&, std::ostream&, dlvhex::dl::Ontology::shared_pointer o);
+	std::string getTransformedProgram();
+};
+
+}} // dlvhex::df
+
+#endif /* _DLVHEX_DF_REWRITER_H */
