5a8b7a0d2c12d9409d33a96672810a7f0696b8bc
Minh Dao-Tran <dao@kr.tuwien.ac.at>
2008-01-28 16:00:33 +0000

    A set of DL Rules.

---
+++ src/DLRules.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file   DLRules.cpp
+ * @author DAO Tran Minh
+ * @date   Tue Dec 18 17:20:24 2007
+ * 
+ * @brief  A set of DL Rules.
+ * 
+ * 
+ */
+
+#include "DLRules.h"
+
+namespace dlvhex {
+namespace df {
+
+DLRules::DLRules() {
+}
+
+void DLRules::addDLRule(const DLRule& dlrule) {
+	dlrules.push_back(dlrule);
+}
+
+void DLRules::insertDLRules(const DLRules& dlrules_) {
+	if (dlrules.size() > 0) {
+		dlrules.insert(dlrules.begin(), dlrules_.dlrules.begin(), dlrules_.dlrules.end());
+	} else {
+		dlrules = dlrules_.dlrules;
+	}
+}
+
+std::string DLRules::toString() {
+	std::string tmp = "";
+	std::vector<DLRule>::iterator pos;
+	if (dlrules.size() > 0) {
+		for (pos = dlrules.begin(); pos != dlrules.end(); pos++) {
+			tmp += pos->toString();
+		}
+	}		
+	return (tmp + "\n");
+}
+
+}	// namespace df
+}	// namespace dlvhe
