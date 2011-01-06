0c5b56b9573505bb3157772bee0e3233c1b7b26e
Peter Schüller <ps@kr.tuwien.ac.at>
2011-01-06 14:55:43 +0000

    attempt to fix racer test on gluck

--- testsuite/RacerStartOrRestart.sh
+++ testsuite/RacerStartOrRestart.sh
@@ -8,6 +8,10 @@ killall -9 $RACER
 
 # start racer in a screen called 'racer' in detached mode
 # connect to it via "screen -x racer"
-screen -S racer -d -m $RACERSTARTSCRIPT
+#screen -S racer -d -m $RACERSTARTSCRIPT
+# screen is not good if user has no homedir and no password and is primarily used via sudo
+
+#start it in background and discard all output
+$RACERSTARTSCRIPT >/dev/null 2>&1 &
 
 exit 0
