06f1cfe1c9eda79941b0ca4f320f9fd605ccfbb1
Christoph Redl <redl@kr.tuwien.ac.at>
2012-01-28 13:58:25 +0000

    fix benchmark script

--- examples/benchmark.sh
+++ examples/benchmark.sh
@@ -74,14 +74,13 @@ do
 	for c in "${confs[@]}"
 	do
 		if [ ${timeout[$i]} -eq 0 ]; then
-			output=`/usr/bin/time -f %e dlvhex $c $1 2>&1 >/dev/null`
+			/usr/bin/time -o time.txt -f %e dlvhex $c $1 2>/dev/null >/dev/null
+			output=`cat time.txt`
 			timeout[$i]=`echo "$output > $4" | bc`
 			if [ ${timeout[$i]} -eq 1 ]; then
 				output=$4
 			fi
 			line="$line   $output"
-		else
-			line="$line   $4"
 		fi
 		let i=i+1
 	done
