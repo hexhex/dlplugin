48615b9b8a378a83fc46e312cba7a08bdeacc941
Christoph Redl <redl@kr.tuwien.ac.at>
2012-02-25 11:45:16 +0000

    add script

---
+++ examples/wine_count.sh
@@ -0,0 +1,22 @@
+# create working directory
+wd=`mktemp -d $PWD/tmp.XXXXXXXXXX`
+
+
+categories=(AlsatianWine AmericanWine Beaujolais Bordeaux Burgundy CabernetFranc CabernetSauvignon CaliforniaWine Chardonnay CheninBlanc DessertWine DryWine EarlyHarvest FrenchWine FullBodiedWine Gamay GermanWine ItalianWine LateHarvest Loire Meritage Merlot PetiteSyrah PinotBlanc PinorNoir RedWine Riesling RoseWine SemillonOrSauvignonBlanc SweetWine TableWine TexasWine WhiteWine Zinfandel)
+
+for category in ${categories[@]}
+do
+
+	echo "
+	#maxint=1000.
+
+	wine(X) :- &dlC[\"wine.rdf\", pcwine, mcwine, prwine, mrwine, \"$category\"](X).
+	cnt(\"$category\", C) :- C = #count{ X : wine(X) }.
+
+" > $wd/prog.hex
+
+	dlvhex2 --silent --plugindir=../src/.libs/ $wd/prog.hex
+done
+
+# cleanup
+rm -r $wd
